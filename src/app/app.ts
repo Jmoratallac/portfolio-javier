6918bae0bf6d7d5927378e7945c59082fda03b38
Jmoratallac <javier13acm@gmail.com>
2026-06-11 12:21:04 +0100

    Finish portfolio design and content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Experience } from './components/experience/experience';
+import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Navbar,
+    Hero,
+    About,
+    Experience,
+    Projects,
+    Skills,
+    Contact,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('portfolio-javier');
-}
+export class App {}
